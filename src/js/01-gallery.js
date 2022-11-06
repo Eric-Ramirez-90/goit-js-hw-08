04f5cc33579c51cdcb9841f2e50a6be7a128bdbb
Eric Ramirez <ramirez1990rer@gmail.com>
2022-11-06 13:02:17 +0200

    Completed task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
-console.log('Hello world!');
+// console.log(galleryItems);
+// console.log(SimpleLightbox);
+
+const refs = {
+  containerForMarkup: document.querySelector('.gallery'),
+};
+
+const createElement = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<div class="gallery__item some-element">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+refs.containerForMarkup.insertAdjacentHTML('beforeend', createElement);
+
+new SimpleLightbox('.some-element a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  navText: ['⮜', '⮞'],
+});
