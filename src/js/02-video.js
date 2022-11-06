90210d03560fd43c1ce426d04f712078fe2972d4
Eric Ramirez <ramirez1990rer@gmail.com>
2022-11-06 15:57:56 +0200

    Completed task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// console.log(Player);
+// console.log(throttle);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onCatchCurrentTime, 1000));
+
+function onCatchCurrentTime(data) {
+  return localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
